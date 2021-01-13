82f5aa728022ff9ca0ed7af1c3cbd030f7809d4c
abhishek <ak6726084@gmail.com>
2021-01-13 22:06:53 +0530

    r added

---
+++ zxde.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+if(i)
+printf("A\t");
+i==i==0;
+if(i)
+ printf("B");
+ 
+//printf("%d",sizeof(printf("p1 exam")));
+//	int x=22;
+//	if(x=12)
+//	printf("true");
+//	int x = printf("p1 exam xfxfdgdgfgsfgsrdgf");
+//	printf("%d",x);
+//	int output;
+	//output=scanf("%d",output);
+//	printf("output");
+	return 0;
+}
