4ab8cc109af6fb631dfa680352401978f2eaa3b4
abhishek <ak6726084@gmail.com>
2021-01-13 23:57:55 +0530

    py added

---
+++ love.py
@@ -0,0 +1,25 @@
+from world import me, you
+from my.consciousness import I          
+#from feeling import
+
+always=True
+if you in (depression,sadness,resignation):
+    print("my sweet heart")
+    if feel(you) is lonley or feel(you) is neglected:
+        talk_me(this)
+
+        while always:
+            I.take_care_of(you)
+            if you.love() is not me:
+                break
+        for emotion in my.heart:
+            try:
+           # your.lover = me
+            expect:
+            your.best_friend = me
+    if you.hate(me):
+        love(me)
+    elif I.annoying():
+        love(me)
+    else: 
+        love(me,how="deeply", when="now")
